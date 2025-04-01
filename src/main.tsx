a46ea38919f2d25e537606d9fffbe80fb594f18e
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-01 09:14:50 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementByClass("page__body")!).render(<App />);
+createRoot(document.getElementByClassName("page__body")!).render(<App />);
