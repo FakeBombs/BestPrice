4cf5dfeaafecffdb845a646a7d7455999f94bfbf
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-01 07:07:14 +0300

    Update SearchResults.tsx

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -77,10 +77,10 @@ const SearchResults = () => {
                     <div class="filters__header-title filters__header-title--filters">Κατηγορίες</div>
                   </div>
                   <ol>
-                    <li><a data-c="2" href="/cat/3446/tablets.html?q=ipad+pro+12.9-inch+m2"><span>Tablets</span></a></li>
-                    <li><a data-c="2" href="/cat/5951/screen-protectors-tablets.html?q=ipad+pro+12.9-inch+m2"><span>Προστασία Οθόνης Tablet</span></a></li>
-                    <li><a data-c="3" href="/cat/5943/thikes-tablet.html?q=ipad+pro+12.9-inch+m2"><span>Θήκες Tablet</span></a></li>
-                    <li><a data-c="1" href="/cat/815/grafides-afis.html?q=ipad+pro+12.9-inch+m2"><span>Γραφίδες Αφής</span></a></li>
+                    <li><a data-c="2" href="/cat/3446/tablets.html?q={query}"><span>Tablets</span></a></li>
+                    <li><a data-c="2" href="/cat/5951/screen-protectors-tablets.html?q={query}"><span>Προστασία Οθόνης Tablet</span></a></li>
+                    <li><a data-c="3" href="/cat/5943/thikes-tablet.html?q={query}"><span>Θήκες Tablet</span></a></li>
+                    <li><a data-c="1" href="/cat/815/grafides-afis.html?q={query}"><span>Γραφίδες Αφής</span></a></li>
                   </ol>
                 </div>
 
@@ -88,12 +88,48 @@ const SearchResults = () => {
                   <div class="filters__header-title filters__header-title--filters">Φίλτρα</div>
                 </div>
 
-                <div class="filter-limit default-list" data-filter-name="limit" data-filter-id="" data-type="" data-key="limit"><div class="filter__header"><h4>Εμφάνιση μόνο</h4></div><div class="filter-container"><ol><li data-filter="certified"><a title="Πιστοποιημένα καταστήματα" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;certified=1"><svg aria-hidden="true" class="icon" width="16" height="16"><use xlink:href="/public/dist/images/icons/icons.svg#icon-certified-16"></use></svg><span>Πιστοποιημένα καταστήματα</span></a></li><li id="filter-nearby" class="nearby-location is-set"><a title="Κοντά μου" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;nearby=1">Κοντά μου (20 χλμ)</a><div class="filter-nearby__options">Επιλογές</div></li><li data-filter="in-stock"><a title="Άμεσα διαθέσιμα" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;instock=1"><span>Άμεσα διαθέσιμα</span></a></li><li data-filter="boxnow"><a title="Παράδοση" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;boxnow=1"><svg aria-hidden="true" class="icon" width="24" height="24"><use xlink:href="/public/dist/images/icons/partners.svg#icon-boxnow"></use></svg><span class="help" data-tooltip-left="" data-tooltip="Προϊόντα από καταστήματα που υποστηρίζουν παράδοση με BOXNOW"><svg aria-hidden="true" class="icon help" width="16" height="16"><use xlink:href="/public/dist/images/icons/icons.svg#icon-info-16"></use></svg></span><span>Παράδοση</span></a></li></ol></div></div>
-                <div class="filter-brand filter-collapsed default-list" data-filter-name="Κατασκευαστής" data-filter-id="1" data-type="brand" data-key="brand"><div class="filter__header"><h4>Κατασκευαστής</h4></div><div class="filter-container"><ol data-total="4" data-hidden="-1"><li><a data-c="4" data-id="9" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;f1=9">Apple</a></li><li><a data-c="2" data-id="20028" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;f1=20028">Spigen</a></li><li><a data-c="1" data-id="128" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;f1=128">Targus</a></li><li><a data-c="1" data-id="39080" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;f1=39080">Techsuit</a></li></ol></div></div>
-                <div class="filter-store filter-collapsed default-list" data-filter-name="Πιστοποιημένα καταστήματα" data-filter-id="store" data-type="store" data-key="store"><div class="filter__header"><h4>Πιστοποιημένα καταστήματα</h4></div><div class="filter-container"><ol data-total="15" data-hidden="10"><li><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=plaisio.gr"><span>Plaisio</span></a></li><li><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=germanos.gr"><span>Germanos</span></a></li><li><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=you.gr"><span>You</span></a></li><li><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=e-gateway.gr"><span>e-Gateway</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=websupplies.gr"><span>Websupplies</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=adaptoras.gr"><span>Adaptoras</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=madhawk.gr"><span>Madhawk</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=plusmobi.net"><span>Plusmobi</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=metrostore.gr"><span>Metrostore</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=twiinshop.gr"><span>Twiinshop</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=electroholic.gr"><span>Electroholic</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=3dmall.gr"><span>3dMall</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=mobicell.gr"><span>MobiCell</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=gadgetmart.gr"><span>GadgetMart</span></a></li><li class="hidden"><a data-l="1" rel="nofollow" href="/search?q=ipad+pro+12.9-inch+m2&amp;store=techstores.gr"><span>Techstores</span></a></li></ol><div id="filter-store-prompt" class="filters-more-prompt" title="Εμφάνιση όλων των πιστοποιημένων καταστημάτων"><svg aria-hidden="true" class="icon" width="100%" height="100%"><use xlink:href="/public/dist/images/icons/icons.svg#icon-plus-more"></use></svg> Εμφάνιση όλων</div></div></div>
+                <div class="filter-limit default-list" data-filter-name="limit" data-filter-id="" data-type="" data-key="limit">
+                  <div class="filter__header">
+                    <h4>Εμφάνιση μόνο</h4>
+                  </div>
+                  <div class="filter-container">
+                    <ol>
+                      <li data-filter="certified">
+                        <a title="Πιστοποιημένα καταστήματα" rel="nofollow" href="/search?q={query}&amp;certified=1">
+                          <svg aria-hidden="true" class="icon" width="16" height="16">
+                            <use xlink:href="//www.bestprice.gr/public/dist/images/icons/icons.svg#icon-certified-16"></use>
+                          </svg>
+                          <span>Πιστοποιημένα καταστήματα</span>
+                        </a>
+                      </li>
+                      <li id="filter-nearby" class="nearby-location is-set">
+                        <a title="Κοντά μου" rel="nofollow" href="/search?q={query}&amp;nearby=1">Κοντά μου (20 χλμ)</a>
+                        <div class="filter-nearby__options">Επιλογές</div>
+                      </li>
+                      <li data-filter="in-stock">
+                        <a title="Άμεσα διαθέσιμα" rel="nofollow" href="/search?q={query}&amp;instock=1"><span>Άμεσα διαθέσιμα</span></a>
+                      </li>
+                      <li data-filter="boxnow">
+                        <a title="Παράδοση" rel="nofollow" href="/search?q={query}&amp;boxnow=1">
+                          <svg aria-hidden="true" class="icon" width="24" height="24">
+                            <use xlink:href="//www.bestprice.gr/public/dist/images/icons/partners.svg#icon-boxnow"></use>
+                          </svg>
+                          <span class="help" data-tooltip-left="" data-tooltip="Προϊόντα από καταστήματα που υποστηρίζουν παράδοση με BOXNOW">
+                            <svg aria-hidden="true" class="icon help" width="16" height="16">
+                              <use xlink:href="//www.bestprice.gr//www.bestprice.gr/public/dist/images/icons/icons.svg#icon-info-16"></use>
+                            </svg>
+                          </span><span>Παράδοση</span>
+                        </a>
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+                
+                <div class="filter-brand filter-collapsed default-list" data-filter-name="Κατασκευαστής" data-filter-id="1" data-type="brand" data-key="brand"><div class="filter__header"><h4>Κατασκευαστής</h4></div><div class="filter-container"><ol data-total="4" data-hidden="-1"><li><a data-c="4" data-id="9" rel="nofollow" href="/search?q={query}&amp;f1=9">Apple</a></li><li><a data-c="2" data-id="20028" rel="nofollow" href="/search?q={query}&amp;f1=20028">Spigen</a></li><li><a data-c="1" data-id="128" rel="nofollow" href="/search?q={query}&amp;f1=128">Targus</a></li><li><a data-c="1" data-id="39080" rel="nofollow" href="/search?q={query}&amp;f1=39080">Techsuit</a></li></ol></div></div>
+                <div class="filter-store filter-collapsed default-list" data-filter-name="Πιστοποιημένα καταστήματα" data-filter-id="store" data-type="store" data-key="store"><div class="filter__header"><h4>Πιστοποιημένα καταστήματα</h4></div><div class="filter-container"><ol data-total="15" data-hidden="10"><li><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=plaisio.gr"><span>Plaisio</span></a></li><li><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=germanos.gr"><span>Germanos</span></a></li><li><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=you.gr"><span>You</span></a></li><li><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=e-gateway.gr"><span>e-Gateway</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=websupplies.gr"><span>Websupplies</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=adaptoras.gr"><span>Adaptoras</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=madhawk.gr"><span>Madhawk</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=plusmobi.net"><span>Plusmobi</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=metrostore.gr"><span>Metrostore</span></a></li><li class="hidden"><a data-l="3" rel="nofollow" href="/search?q={query}&amp;store=twiinshop.gr"><span>Twiinshop</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q={query}&amp;store=electroholic.gr"><span>Electroholic</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q={query}&amp;store=3dmall.gr"><span>3dMall</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q={query}&amp;store=mobicell.gr"><span>MobiCell</span></a></li><li class="hidden"><a data-l="2" rel="nofollow" href="/search?q={query}&amp;store=gadgetmart.gr"><span>GadgetMart</span></a></li><li class="hidden"><a data-l="1" rel="nofollow" href="/search?q={query}&amp;store=techstores.gr"><span>Techstores</span></a></li></ol><div id="filter-store-prompt" class="filters-more-prompt" title="Εμφάνιση όλων των πιστοποιημένων καταστημάτων"><svg aria-hidden="true" class="icon" width="100%" height="100%"><use xlink:href="//www.bestprice.gr/public/dist/images/icons/icons.svg#icon-plus-more"></use></svg> Εμφάνιση όλων</div></div></div>
                 <div class="filters__buttons"><button class="button" data-filters-close="">Εμφάνιση  των  προϊόντων</button></div>
-                <div class="filters__md-toggler" hidden="" role="button"><h5><svg aria-hidden="true" class="icon" width="18" height="18"><use xlink:href="/public/dist/images/icons/icons.svg#icon-filters-18"></use></svg>ΦΙΛΤΡΑ</h5></div>
-                <button class="button button--outline" id="filters__scrollback" style="left: 194px; width: 240px;"><svg class="icon" aria-hidden="true" width="12" height="12"><use xlink:href="/public/dist/images/icons/icons.svg#icon-up-12"></use></svg><div>Φίλτρα</div></button>
+                <div class="filters__md-toggler" hidden="" role="button"><h5><svg aria-hidden="true" class="icon" width="18" height="18"><use xlink:href="//www.bestprice.gr/public/dist/images/icons/icons.svg#icon-filters-18"></use></svg>ΦΙΛΤΡΑ</h5></div>
+          
                 
               </div>
             </aside>
@@ -149,7 +185,7 @@ const SearchResults = () => {
               <div class="alerts">
                 <button data-url="/search?q={query}" data-title="{query}" data-max-price="0" class="alerts__button pressable">
                   <svg aria-hidden="true" class="icon" width="20" height="20">
-                    <use xlink:href="/public/dist/images/icons/icons.svg#icon-notification-outline-20"></use>
+                    <use xlink:href="//www.bestprice.gr/public/dist/images/icons/icons.svg#icon-notification-outline-20"></use>
                   </svg>
                   <span class="alerts__label">Ειδοποίηση</span>
                 </button>
