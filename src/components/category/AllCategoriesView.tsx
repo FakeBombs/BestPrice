9cca99b25f9bdc6c78883f5d4c38625fd87d198c
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-04 00:26:30 +0300

    Update AllCategoriesView.tsx

--- src/components/category/AllCategoriesView.tsx
+++ src/components/category/AllCategoriesView.tsx
@@ -13,7 +13,6 @@ const AllCategoriesView = ({ rootCategories, categories }: AllCategoriesViewProp
   return (
     <div className="root__wrapper root-category__root">
       <div className="root">
-        <div id="trail"><CategoryBreadcrumb rootCategories={rootCategory} /></div>
         <div className="root-category__categories">
               {rootCategories.map((rootCat) => (
                 <div className="root-category__category" key={rootCat.id}>
@@ -23,33 +22,6 @@ const AllCategoriesView = ({ rootCategories, categories }: AllCategoriesViewProp
         </div>
       </div>
     </div>
-
-
-
-
-
-
-
-
-
-
-<>
-      
-      {/* Root Categories */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-12">
-        {rootCategories.map((rootCat) => (
-          <RootCategoryCard key={rootCat.id} category={rootCat} />
-        ))}
-      </div>
-      
-      {/* Subcategories */}
-      <h2 className="text-2xl font-semibold mb-6">All Subcategories</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-        {categories.map((category) => (
-          <CategoryCard key={category.id} category={category} />
-        ))}
-      </div>
-  </>
   );
 };
 
