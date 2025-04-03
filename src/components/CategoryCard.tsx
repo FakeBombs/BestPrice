5c1a9babe8d66b58757d4a78f1b1025cd51b081d
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-03 21:05:18 +0300

    Update CategoryCard.tsx

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -2,7 +2,9 @@ import { Link } from 'react-router-dom';
 import { Card, CardContent } from '@/components/ui/card';
 import { Smartphone, Laptop, Tablet, Headphones, Camera, Monitor, Home, Gamepad, LucideIcon } from 'lucide-react';
 import { Category } from '@/data/mockData';
-
+interface CategoryCardProps {
+  category: Category;
+}
 const CategoryCard = ({
   category
 }: CategoryCardProps) => {
