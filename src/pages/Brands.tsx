3c42bae65059c12078fa8d2bdf359e8b896481b1
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-07 22:08:11 +0300

    Update Brands.tsx

--- src/pages/Brands.tsx
+++ src/pages/Brands.tsx
@@ -33,7 +33,7 @@ const Brands = () => {
         <header class="page-header">
           <h1>37.353 κατασκευαστές</h1>
           <span class="autocomplete__wrapper" style={{display: "inline-block", position: "relative", verticalAlign: "top", zIndex: "500000000"}}>
-            <input type="search" id="brand-search-q" placeholder="Γρήγορη εύρεση ..." autocomplete="off" autocorrect="off" spellcheck="false">
+            <input type="search" id="brand-search-q" placeholder="Γρήγορη εύρεση ..." autocomplete="off" autocorrect="off" spellcheck="false"/>
               <div class="autocomplete autocomplete--minimal" style={{display: "none"}}></div>
           </span>
         </header>
