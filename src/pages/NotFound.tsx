df52f43da5b59842f3182d8c54d8c03be30c1171
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-07 20:44:46 +0300

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,7 @@
 
 import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
+import { useTranslation } from '@/hooks/useTranslation';
 
 const NotFound = () => {
   return (
