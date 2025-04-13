09fef884bc2ba38d0f1acec20fbb267fd3624fef
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-13 03:46:16 +0300

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-
+import React, { useState } from 'react';
 import { ReactNode } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
@@ -8,10 +8,25 @@ interface MainLayoutProps {
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
+  const [isSitemapVisible, setIsSitemapVisible] = useState(false);
+
+  // Function to toggle the visibility of the popup menu
+  const toggleSitemapVisibility = () => {
+    setIsSitemapVisible(prev => !prev);
+  };
   return (
     <div>
-      <Navbar />
+      <Navbar onToggleSitemap={toggleSitemapVisibility} />
       <div id="root" className="clr">
+        {isSitemapVisible && (
+          <div class="sitemap-desktop__backdrop" style={{ zIndex: 2147483524 }}></div>
+          <div class="sitemap-desktop__wrapper" style={{ zIndex: 2147483525 }}>
+            <div class="root__wrapper">
+              <div class="root"></div>
+            </div>
+          </div>
+        )}
+        
         {children}
       </div>
       <Footer />
