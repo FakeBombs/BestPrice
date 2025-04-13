5a8e6ec64ea332e1e00509c709004db6c2b8cf64
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-13 03:56:11 +0300

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { ReactNode } from 'react';
+import React, { useState, ReactNode } from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
