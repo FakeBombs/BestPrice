d2850872f1bfe623786b247cd294e0bbcacd3a37
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-14 15:03:48 +0300

    Update ProductBreadcrumb.tsx

--- src/components/product/ProductBreadcrumb.tsx
+++ src/components/product/ProductBreadcrumb.tsx
@@ -39,29 +39,31 @@ const ProductBreadcrumb = ({ product }: ProductBreadcrumbProps) => {
       <span className="text-foreground truncate max-w-[200px]">{product.title}</span>
     </div>
 
-    <div class="comparison__placeholder">
-    <div class="comparison__category">
-      <div class="comparison__category-products">
-        <div class="tooltip__anchor comparison__category-product">
-          <div class="comparison__category-remove">
-            <svg class="icon icon--no-margin" aria-hidden="true" width="20" height="20"><use xlink:href="/public/dist/images/icons/icons.svg#icon-round-x-20"></use></svg>
+    <>
+    <div className="comparison__placeholder">
+      <div className="comparison__category">
+        <div className="comparison__category-products">
+          <div className="tooltip__anchor comparison__category-product">
+            <div className="comparison__category-remove">
+              <svg className="icon icon--no-margin" aria-hidden="true" width="20" height="20"><use xlink:href="/public/dist/images/icons/icons.svg#icon-round-x-20"></use></svg>
+            </div>
+            <a href="/item/2158420121/tcl-406-32gb.html?bpref=comparison-category">
+              <img width="28" height="28" loading="lazy" alt="TCL 406 32GB" srcset="//bbpcdn.pstatic.gr/bpimg25/2m4vFL/1QXN8K_SX56Y56/1698752554/tcl-406-32gb.webp 2x" src="//bbpcdn.pstatic.gr/bpimg25/2m4vFL/1QXN8K_SX28Y28/1698752554/tcl-406-32gb.webp"/>
+            </a>
           </div>
-          <a href="/item/2158420121/tcl-406-32gb.html?bpref=comparison-category">
-            <img width="28" height="28" loading="lazy" alt="TCL 406 32GB" srcset="//bbpcdn.pstatic.gr/bpimg25/2m4vFL/1QXN8K_SX56Y56/1698752554/tcl-406-32gb.webp 2x" src="//bbpcdn.pstatic.gr/bpimg25/2m4vFL/1QXN8K_SX28Y28/1698752554/tcl-406-32gb.webp"/>
-          </a>
-        </div>
-        <div class="tooltip__anchor comparison__category-product">
-          <div class="comparison__category-remove">
-            <svg class="icon icon--no-margin" aria-hidden="true" width="20" height="20"><use xlink:href="/public/dist/images/icons/icons.svg#icon-round-x-20"></use></svg>
+          <div className="tooltip__anchor comparison__category-product">
+            <div className="comparison__category-remove">
+              <svg className="icon icon--no-margin" aria-hidden="true" width="20" height="20"><use xlink:href="/public/dist/images/icons/icons.svg#icon-round-x-20"></use></svg>
+            </div>
+            <a href="/item/2159422084/realme-note-50-4gb-128gb.html?bpref=comparison-category">
+              <img width="28" height="28" loading="lazy" alt="Realme Note 50 4GB 128GB" srcset="//bbpcdn.pstatic.gr/bpimg21/aeRAp/1SYzV1_SX56Y56/1728492731/realme-note-50-4gb-128gb.webp 2x" src="//bbpcdn.pstatic.gr/bpimg21/aeRAp/1SYzV1_SX28Y28/1728492731/realme-note-50-4gb-128gb.webp"/>
+            </a>
           </div>
-          <a href="/item/2159422084/realme-note-50-4gb-128gb.html?bpref=comparison-category">
-            <img width="28" height="28" loading="lazy" alt="Realme Note 50 4GB 128GB" srcset="//bbpcdn.pstatic.gr/bpimg21/aeRAp/1SYzV1_SX56Y56/1728492731/realme-note-50-4gb-128gb.webp 2x" src="//bbpcdn.pstatic.gr/bpimg21/aeRAp/1SYzV1_SX28Y28/1728492731/realme-note-50-4gb-128gb.webp"/>
-          </a>
         </div>
+        <a href="/compare/2158420121-2159422084" className="button button--outline comparison__category-link">Σύγκριση (2)</a>
       </div>
-      <a href="/compare/2158420121-2159422084" class="button button--outline comparison__category-link">Σύγκριση (2)</a>
     </div>
-    </div>
+    </>
   );
 };
 
