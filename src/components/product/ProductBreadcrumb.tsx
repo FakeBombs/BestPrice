7514692eef98be72acdea7e8414f3bf8fab34131
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-14 14:59:23 +0300

    Update ProductBreadcrumb.tsx

--- src/components/product/ProductBreadcrumb.tsx
+++ src/components/product/ProductBreadcrumb.tsx
@@ -1,4 +1,3 @@
-
 import { Link } from 'react-router-dom';
 import { ChevronRight } from 'lucide-react';
 import { Product, categories } from '@/data/mockData';
@@ -38,6 +37,30 @@ const ProductBreadcrumb = ({ product }: ProductBreadcrumbProps) => {
       <Link to={`/cat/${categoryId}/${categorySlug}`} className="text-muted-foreground hover:text-primary">{product.category}</Link>
       <ChevronRight className="h-4 w-4 mx-1" />
       <span className="text-foreground truncate max-w-[200px]">{product.title}</span>
+    </div>
+
+    <div class="comparison__placeholder">
+    <div class="comparison__category">
+      <div class="comparison__category-products">
+        <div class="tooltip__anchor comparison__category-product">
+          <div class="comparison__category-remove">
+            <svg class="icon icon--no-margin" aria-hidden="true" width="20" height="20"><use xlink:href="/public/dist/images/icons/icons.svg#icon-round-x-20"></use></svg>
+          </div>
+          <a href="/item/2158420121/tcl-406-32gb.html?bpref=comparison-category">
+            <img width="28" height="28" loading="lazy" alt="TCL 406 32GB" srcset="//bbpcdn.pstatic.gr/bpimg25/2m4vFL/1QXN8K_SX56Y56/1698752554/tcl-406-32gb.webp 2x" src="//bbpcdn.pstatic.gr/bpimg25/2m4vFL/1QXN8K_SX28Y28/1698752554/tcl-406-32gb.webp"/>
+          </a>
+        </div>
+        <div class="tooltip__anchor comparison__category-product">
+          <div class="comparison__category-remove">
+            <svg class="icon icon--no-margin" aria-hidden="true" width="20" height="20"><use xlink:href="/public/dist/images/icons/icons.svg#icon-round-x-20"></use></svg>
+          </div>
+          <a href="/item/2159422084/realme-note-50-4gb-128gb.html?bpref=comparison-category">
+            <img width="28" height="28" loading="lazy" alt="Realme Note 50 4GB 128GB" srcset="//bbpcdn.pstatic.gr/bpimg21/aeRAp/1SYzV1_SX56Y56/1728492731/realme-note-50-4gb-128gb.webp 2x" src="//bbpcdn.pstatic.gr/bpimg21/aeRAp/1SYzV1_SX28Y28/1728492731/realme-note-50-4gb-128gb.webp"/>
+          </a>
+        </div>
+      </div>
+      <a href="/compare/2158420121-2159422084" class="button button--outline comparison__category-link">Σύγκριση (2)</a>
+    </div>
     </div>
   );
 };
