ff5ac854194ca512a7d49a8b994eaa11afdba08a
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-15 10:35:13 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Routes, Route, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import AdminLayout from "./pages/admin/AdminLayout";
 import Index from "./pages/Index";
