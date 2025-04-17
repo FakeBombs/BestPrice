23814a6d292b86474724bb04d1e9082b62276de0
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-17 20:30:11 +0300

    Update RootCategoryCard.tsx

--- src/components/RootCategoryCard.tsx
+++ src/components/RootCategoryCard.tsx
@@ -8,15 +8,34 @@ interface RootCategoryCardProps {
 
 const RootCategoryCard = ({ category }: RootCategoryCardProps) => {
   return (
-    <>
-      <Link to={`/categories/root/${category.slug}?bpref=root-category`} className="root-category__cover">
-        <img src={category.image} alt={category.name} title={category.name} className="root-category__image" />
-      </Link>
-      <h2 className="root-category__category-title">
-        <Link to={`/categories/root/${category.slug}?bpref=root-category__title`}>{category.name}</Link>
-      </h2>
-      <p className="text-muted-foreground text-sm line-clamp-2">{category.description}</p>
-    </>
+    <Card className="root-category__category">
+      <CardContent>
+        <Link to={`/cat/${category.slug}?bpref=root-category`} className="root-category__cover">
+          <img 
+            src={category.image} 
+            alt={category.name} 
+            title={category.name} 
+            className="root-category__image" 
+          />
+        </Link>
+        <h2 className="root-category__category-title">
+          <Link to={`/cat/${category.slug}?bpref=root-category__title`}>{category.name}</Link>
+        </h2>
+        <div className="root-category__footer">
+          <div className="root-category__links">
+            {category.categories.map((subCategory) => (
+              <Link 
+                key={subCategory.id} 
+                to={`/cat/${subCategory.slug}?bpref=root-category-subcat`} 
+              >
+                {subCategory.name}
+              </Link>
+            )).reduce((prev, curr) => [prev, ', ', curr])} 
+            {/* The reduce adds commas between links */}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
