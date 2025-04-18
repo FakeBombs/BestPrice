9b618c41d7108016ccb79ac75c93b340dfab245a
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-18 18:22:38 +0300

    Update ProductHighlights.tsx

--- src/components/product/ProductHighlights.tsx
+++ src/components/product/ProductHighlights.tsx
@@ -8,7 +8,7 @@ const ProductHighlights = ({ specifications }: ProductHighlightsProps) => {
   return (
     <div className="item-header__content">
       <ul className="item-header__specs-list">
-        {Object.entries(specifications).slice(0, 4).map(([key, value]) => (
+        {Object.entries(specifications).slice(0, 8).map(([key, value]) => (
           <li key={key}>{key}: {value}</li>
         ))}
         <li data-no-bullet=""><a className="dotted-link" href="/item/2160473294/samsung-galaxy-a56-8gb-256gb.html#item-specs">Περισσότερα</a></li>
