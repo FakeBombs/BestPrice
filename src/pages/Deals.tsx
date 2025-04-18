f477c90049b684578d78f486a51ff3887f5d95ae
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-18 06:02:02 +0300

    Update Deals.tsx

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { fetchDeals, mainCategories } from '@/data/mockData'; 
 import ProductCard from '@/components/ProductCard';
 import ProductFilter from '@/components/ProductFilter';
