63d98b0e1c4f56dec03b05ab15b8dea12f641a82
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-19 05:30:05 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,8 +35,7 @@ function App() {
           <Route path="item/:productId/:productSlug?" element={<ProductDetail />} />
           <Route path="category/:categorySlug" element={<Categories />} />
           <Route path="categories" element={<Categories />} />
-          <Route path="/cat/:categoryId/:rootCategorySlug" component={CategoryPage} /> {/* New Route for Category using ID and Slug */}
-          <Route path="/cat/:categoryId/:rootCategorySlug.html" component={CategoryPage} /> {/* New Route for Category using ID and Slug */}
+          <Route path="/cat/:categoryId/:slug" element={<CategoryPage />} /> {/* New Route for Category using ID and Slug */}
           <Route path="brands" element={<Brands />} />
           <Route path="stores" element={<Stores />} />
           <Route path="deals" element={<Deals />} />
