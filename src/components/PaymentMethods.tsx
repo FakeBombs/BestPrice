d162144badeea5fe092f95faf071a3a6de9a1286
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-21 19:42:29 +0300

    Update PaymentMethods.tsx

--- src/components/PaymentMethods.tsx
+++ src/components/PaymentMethods.tsx
@@ -1,15 +1,20 @@
-// PaymentMethods.js
 const PaymentMethod = {
-    COD: 'Αντικαταβολή',
-    CreditCard: 'Πιστωτικές κάρτες',
-    PayPal: 'PayPal',
-    BankTransfer: 'Τραπεζική κατάθεση',
-    PickupVia: 'Παραλαβή από δίκτυο',
-    FreeReturn: 'Δωρεάν επιστροφή',
-    PointsCollection: 'Συλλογή πόντων',
-    GiftCards: 'Δωροκάρτες',
-    ExtendedWarranty: 'Επέκταση εγγύησης',
-    DeviceRecycling: 'Ανακύκλωση συσκευών',
+  COD = 'Αντικαταβολή',
+  CreditCard = 'Πιστωτικές κάρτες',
+  PayPal = 'PayPal',
+  BankTransfer = 'Τραπεζική κατάθεση',
+  VivaPayments = 'Viva Payments',
+  Paysafecard = 'Paysafecard',
+  Courier = 'Ταχυμεταφορά (Courier)',
+  NetworkPickup = 'Παραλαβή από δίκτυο',
+  TransportCompany = 'Μεταφορική εταιρία',
+  PickupVia = 'Παραλαβή μέσω',
+  FreeReturn = 'Δωρεάν επιστροφή',
+  PointsCollection = 'Συλλογή πόντων',
+  GiftCards = 'Δωροκάρτες',
+  ExtendedWarranty = 'Επέκταση εγγύησης',
+  WeddingList = 'Λίστα γάμου/μωρού',
+  DeviceRecycling = 'Ανακύκλωση συσκευών',
 };
 
 const allPaymentMethods = Object.values(PaymentMethod);
