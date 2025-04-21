828338e26c208a4620d8be2ac574cee93e379813
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-21 19:47:31 +0300

    Update PaymentMethods.tsx

--- src/components/PaymentMethods.tsx
+++ src/components/PaymentMethods.tsx
@@ -1,20 +1,53 @@
+import React from 'react';
+
 const PaymentMethod = {
-  COD = 'Αντικαταβολή',
-  CreditCard = 'Πιστωτικές κάρτες',
-  PayPal = 'PayPal',
-  BankTransfer = 'Τραπεζική κατάθεση',
-  VivaPayments = 'Viva Payments',
-  Paysafecard = 'Paysafecard',
-  Courier = 'Ταχυμεταφορά (Courier)',
-  NetworkPickup = 'Παραλαβή από δίκτυο',
-  TransportCompany = 'Μεταφορική εταιρία',
-  PickupVia = 'Παραλαβή μέσω',
-  FreeReturn = 'Δωρεάν επιστροφή',
-  PointsCollection = 'Συλλογή πόντων',
-  GiftCards = 'Δωροκάρτες',
-  ExtendedWarranty = 'Επέκταση εγγύησης',
-  WeddingList = 'Λίστα γάμου/μωρού',
-  DeviceRecycling = 'Ανακύκλωση συσκευών',
+  COD: 'Αντικαταβολή',
+  CreditCard: 'Πιστωτικές κάρτες',
+  PayPal: 'PayPal',
+  BankTransfer: 'Τραπεζική κατάθεση',
+  VivaPayments: 'Viva Payments',
+  Paysafecard: 'Paysafecard',
+  Courier: 'Ταχυμεταφορά (Courier)',
+  NetworkPickup: 'Παραλαβή από δίκτυο',
+  TransportCompany: 'Μεταφορική εταιρία',
+  PickupVia: 'Παραλαβή μέσω',
+  FreeReturn: 'Δωρεάν επιστροφή',
+  PointsCollection: 'Συλλογή πόντων',
+  GiftCards: 'Δωροκάρτες',
+  ExtendedWarranty: 'Επέκταση εγγύησης',
+  WeddingList: 'Λίστα γάμου/μωρού',
+  DeviceRecycling: 'Ανακύκλωση συσκευών',
 };
 
 const allPaymentMethods = Object.values(PaymentMethod);
+
+const PaymentMethodsComponent = ({ paymentMethods }) => {
+    return (
+        <div className="merchant__shipping-options">
+            <div>
+                <h4 className="ui-kit__text ui-kit__strong ui-kit__mb-3">Τρόποι πληρωμής</h4>
+                <ol>
+                    {allPaymentMethods.map((method, index) => {
+                        const isSupported = paymentMethods.includes(method);
+                        return (
+                            <li className={isSupported ? 'supported' : ''} key={index}>
+                                <span className="ui-kit__text">{method}</span>
+                                {isSupported ? (
+                                    <span className="check-icon--supported">
+                                        <svg aria-hidden="true" className="icon" width="16" height="16">
+                                            <use xlinkHref="/public/dist/images/icons/icons.svg#icon-check-16"></use>
+                                        </svg>
+                                    </span>
+                                ) : (
+                                    <span>—</span>
+                                )}
+                            </li>
+                        );
+                    })}
+                </ol>
+            </div>
+        </div>
+    );
+};
+
+export default PaymentMethodsComponent;
