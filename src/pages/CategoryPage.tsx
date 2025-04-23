af78f63f5b10075fa23211c0009cc885af099861
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-23 04:04:27 +0300

    Update CategoryPage.tsx

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -92,34 +92,35 @@ const CategoryPage: React.FC = () => {
           <h1>{currentCategory?.name}</h1>
         </div>
       </div>
-              {mainCategories.map((mainCat) => (
-      <div className="root-category__categories">
-          <div key={mainCat.id} className="root-category__category">
-            <h2 className="root-category__category-title">{mainCat.name}</h2>
-            {categories
-              .filter(subCat => subCat.parentId === mainCat.id) // Filter to only show subcategories of the current mainCat
-              .map((subCat) => (
-                <div key={subCat.id} className="root-category__subcategory">
-                  <Link to={`/cat/${mainCat.id}/${mainCat.slug}/${subCat.slug}`} className="root-category__cover"><img src={subCat.image} alt={subCat.name} title={subCat.name} /></Link>
-                  <h3 className="root-category__subcategory-title"><Link to={`/cat/${mainCat.id}/${mainCat.slug}/${subCat.slug}`}>{subCat.name}</Link></h3>
-                  <div className="root-category__footer">
-                    <div className="root-category__links">
-                      {categories
-                        .filter(linkedSubCat => linkedSubCat.parentId === subCat.id) // Filter linked subcategories for the current subCat
-                        .slice(0, 3) // Limit to 3 linked subcategories
-                        .map((linkedSubCat, index, arr) => (
-                          <React.Fragment key={linkedSubCat.id}>
-                            <Link to={`/cat/${mainCat.id}/${mainCat.slug}/${linkedSubCat.slug}`}>{linkedSubCat.name}</Link>
-                            {index < arr.length - 1 && ', '} {/* Add comma if there are more */}
-                          </React.Fragment>
-                        ))}
-                    </div>
-                  </div>
-                </div>
-              ))}
-          </div>
+       <div className="root-category__categories">
+        {subcategories.length > 0 ? (
+  subcategories.map((subCat) => (
+    <div key={subCat.id} className="root-category__subcategory">
+      <Link to={`/cat/${subCat.parentId}/${subCat.parentSlug}/${subCat.slug}`} className="root-category__cover">
+        <img src={subCat.image} alt={subCat.name} title={subCat.name} />
+      </Link>
+      <h3 className="root-category__subcategory-title">
+        <Link to={`/cat/${subCat.parentId}/${subCat.parentSlug}/${subCat.slug}`}>{subCat.name}</Link>
+      </h3>
+      <div className="root-category__footer">
+        <div className="root-category__links">
+          {categories
+            .filter(linkedSubCat => linkedSubCat.parentId === subCat.id) // Filter linked subcategories for the current subCat
+            .slice(0, 3) // Limit to 3 linked subcategories
+            .map((linkedSubCat, index, arr) => (
+              <React.Fragment key={linkedSubCat.id}>
+                <Link to={`/cat/${subCat.parentId}/${subCat.parentSlug}/${linkedSubCat.slug}`}>{linkedSubCat.name}</Link>
+                {index < arr.length - 1 && ', '} {/* Add comma if there are more */}
+              </React.Fragment>
+            ))}
+        </div>
       </div>
-        ))}
+    </div>
+  ))
+) : (
+  <div>No subcategories available.</div> // Optional: Message if there are no subcategories
+)}
+      </div>
     </div>
   );
 };
