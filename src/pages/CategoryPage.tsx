1112e16d4dc079f9de8514634abbba350c300b08
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-04-24 19:24:17 +0300

    Update CategoryPage.tsx

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -30,10 +30,8 @@ const CategoryPage: React.FC = () => {
   useEffect(() => {
     if (!currentCategory) return;
 
-    console.log('Current Category:', currentCategory);
-    console.log('All Products:', products);
-
     const isLeafCategory = categories.some(cat => cat.parentId === currentCategory.id);
+    
     if (!isLeafCategory) {
         setFilteredProducts([]); 
     } else {
@@ -41,10 +39,9 @@ const CategoryPage: React.FC = () => {
             product.categoryIds.includes(currentCategory.id)
         );
 
-        console.log('Filtered Products:', productsToDisplay); // Log filtered results
         setFilteredProducts(productsToDisplay);
     }
-}, [currentCategory, products]);
+}, [currentCategory, products]); 
 
     const isLeafCategory = categories.some(cat => cat.parentId === currentCategory.id);
 
