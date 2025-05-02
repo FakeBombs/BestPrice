01cf9fd30335b2aea0e68cb0d3de7e97021079df
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-02 06:13:51 +0300

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './App.css';
+import './index.css';
+import { Providers } from "./providers";
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Providers>
+      <App />
+    </Providers>
+  </React.StrictMode>,
+);
