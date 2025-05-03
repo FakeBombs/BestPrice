e66ee14608a7963ef1c4b9fdd91ff8647ab57728
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-03 23:12:14 +0300

    Update SearchResults.tsx

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -292,11 +292,6 @@ const SearchResults: React.FC = () => {
          {baseSearchResults.length > 0 && (
             <aside className="page-products__filters">
             <div id="filters" role="complementary" aria-labelledby="filters-header">
-              <div className="filters__header">
-                <h3 className="filters__header-title filters__header-title--filters">Φίλτρα</h3>
-                {isAnyFilterActive && ( <Link to="#" onClick={(e) => handleLinkFilterClick(e, handleResetFilters)} className="pressable filters__header-remove popup-anchor" data-tooltip="Αφαίρεση όλων των φίλτρων" data-tooltip-no-border="" data-tooltip-small="true">Καθαρισμός</Link> )}
-              </div>
-
                {/* Categories Filter (Specific to Search - Links to Category Page) */}
                {availableCategories.length > 0 && (
                     <div className="filters__categories filter-categories default-list" data-filter-name="categories">
@@ -321,6 +316,11 @@ const SearchResults: React.FC = () => {
                       </div>
                     </div>
                )}
+              
+              <div className="filters__header">
+                <h3 className="filters__header-title filters__header-title--filters">Φίλτρα</h3>
+                {isAnyFilterActive && ( <Link to="#" onClick={(e) => handleLinkFilterClick(e, handleResetFilters)} className="pressable filters__header-remove popup-anchor" data-tooltip="Αφαίρεση όλων των φίλτρων" data-tooltip-no-border="" data-tooltip-small="true">Καθαρισμός</Link> )}
+              </div>
 
               {/* "Εμφάνιση μόνο" Section */}
               <div className="filter-limit default-list" data-filter-name="limit" data-filter-id data-type data-key="limit">
