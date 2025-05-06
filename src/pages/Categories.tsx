6cd4b6442592e9f6a9b2ed0cb70365e2ea6f7192
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-06 12:25:17 +0300

    Update Categories.tsx

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo, useRef } from 'react';
+import React, { useEffect, useState, useMemo, useRef, useCallback  } from 'react';
 import { useLocation, Link, useSearchParams, useNavigate } from 'react-router-dom';
 import { useToast } from '@/hooks/use-toast';
 import { useAuth } from '@/hooks/useAuth';
