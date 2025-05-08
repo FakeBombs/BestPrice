a41f0c9c3fa8e26eb3e7f9e63bb8349e26079fe9
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-08 05:59:07 +0300

    Update Gifts.tsx

--- src/pages/Gifts.tsx
+++ src/pages/Gifts.tsx
@@ -167,8 +167,8 @@ const Gifts: React.FC = () => {
                             </select>
                             <div className="sc-dKKIkQ bqWucv"> {/* Other filters */}
                                 <label className="bhTOi9I4I2nIIKPU_JUz">
-                                    <input type="checkbox" checked={showDealsOnly} onChange={handleDealsToggle} className="mr-1"/>
                                     {t('deals_label', 'Deals')} ({currentDealCount})
+                                    <input type="checkbox" checked={showDealsOnly} onChange={handleDealsToggle} className="mr-1"/>
                                 </label>
                                 <select value={selectedPriceMax} onChange={handlePriceChange} aria-label={t('price_filter_label', 'Price')}>
                                     <option value="">{t('price_filter_all_label', 'All Prices')}</option>
