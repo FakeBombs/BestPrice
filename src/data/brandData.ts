986716b34144172491c5b410ef10e5e7a6bbef47
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-10 01:27:50 +0300

    Update brandData.ts

--- src/data/brandData.ts
+++ src/data/brandData.ts
@@ -1,1 +1,10 @@
-
+// Brand Interface
+export interface Brand {
+  id: number;
+  name: string;
+  logo: string;
+  slug?: string;
+  officialWebsite?: string;
+  description?: string;
+  countryOfOrigin?: string;
+}
