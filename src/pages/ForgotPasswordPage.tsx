8f17b1f6eaae52777508a470d9f13bf18b1ab53b
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-10 02:06:39 +0300

    Update ForgotPasswordPage.tsx

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -1,4 +1,3 @@
-
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import ForgotPasswordForm from "@/components/ForgotPasswordForm";
 import { useNavigate } from "react-router-dom";
