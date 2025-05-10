85da62a49ea51984863cf55969a4cdcbc509bd7b
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-10 03:15:44 +0300

    Update InlineProductItem.tsx

--- src/components/InlineProductItem.tsx
+++ src/components/InlineProductItem.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
-import { Product, ProductPrice } from '@/data/mockData';
+import { Product, ProductPrice } from '@/data/productData';
 
 interface InlineProductItemProps {
     product: Product;
