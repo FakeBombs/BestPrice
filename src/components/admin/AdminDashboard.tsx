3812e8f0eb10a1365dd4cde801ced9c31dcc80cd
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-10 03:05:31 +0300

    Update AdminDashboard.tsx

--- src/components/admin/AdminDashboard.tsx
+++ src/components/admin/AdminDashboard.tsx
@@ -1,18 +1,8 @@
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { 
-  Package, 
-  LayoutGrid, 
-  ShoppingBag, 
-  Users, 
-  Settings, 
-  BarChart, 
-  Store, 
-  Globe
-} from "lucide-react";
+import { Package, LayoutGrid, ShoppingBag, Users, Settings, BarChart, Store, Globe } from "lucide-react";
 import AdminHeader from "./AdminHeader";
 import { useTranslation } from "@/hooks/useTranslation";
 
