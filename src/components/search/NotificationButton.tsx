d9757dd6377195aa242ceff085bbf531f399a602
FakeBombs <73979254+FakeBombs@users.noreply.github.com>
2025-05-10 03:16:55 +0300

    Update NotificationButton.tsx

--- src/components/search/NotificationButton.tsx
+++ src/components/search/NotificationButton.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 interface NotificationButtonProps {
@@ -10,7 +9,7 @@ const NotificationButton = ({ query }: NotificationButtonProps) => {
     <div className="alerts">
       <button data-url={`/search?q=${query}`} data-title={query} data-max-price="0" className="alerts__button pressable">
         <svg aria-hidden="true" className="icon" width="20" height="20">
-          <use xlinkHref="//www.bestprice.gr/public/dist/images/icons/icons.svg#icon-notification-outline-20"></use>
+          <use href="/dist/images/icons/icons.svg#icon-notification-outline-20"></use>
         </svg>
         <span className="alerts__label">Ειδοποίηση</span>
       </button>
